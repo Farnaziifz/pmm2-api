b73168822b87fc4809aba39864d0c31c7aeca51f
farnazii <ffarzipour92@gmail.com>
2021-08-24 22:34:00 +0430

    change product model

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -3,11 +3,13 @@ export class CreateProductDTO {
   readonly name: string;
   readonly ename: string;
   readonly image: string;
+  readonly image_1: string;
+  readonly image_2: string;
   readonly category_name: string;
   readonly category_id: string;
   readonly brand_id: string;
   readonly brand_name: string;
-  readonly rate: number;
+  readonly uniqueId: number;
   readonly mount: string;
   readonly type: string;
   readonly price: number;
--- src/product/product.interface.ts
+++ src/product/product.interface.ts
@@ -4,11 +4,13 @@ export interface Products extends Document {
   readonly name: string;
   readonly ename: string;
   readonly image: string;
+  readonly image_1: string;
+  readonly image_2: string;
   readonly category_name: string;
   readonly category_id: string;
   readonly brand_id: string;
   readonly brand_name: string;
-  readonly rate: number;
+  readonly uniqueId: number;
   readonly mount: string;
   readonly type: string;
   readonly price: number;
